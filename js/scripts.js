ed4968d24e18e4e8f56b2928aaf03d425fa481a1
mdhernandez <md.hernandez1992@hotmail.com>
2017-12-20 20:52:51 -0500

    I2: Added Javascript Interactions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
 	
+	$('#password').focus(function(){
+		$('#pass-clicked').slideDown();
+	});
+	
+	$('#phone').focus(function(){
+		
+		$('#call-rates').show();
+		$('#phone-clicked').show();
+		$('#phone-blur').hide();
+	});
+	
+	$('#phone').blur(function(){
+		
+		$('#phone-clicked').hide();
+		$('#call-rates').hide();
+		$('#phone-blur').show();
+	});
+	
+	$('#password').blur(function(){
+		$('#pass-clicked').css({"color" : "#eb3432"});
+	});
+	
 	
 });
